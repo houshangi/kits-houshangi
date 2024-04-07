972160f8865873b7165e9d4a40276c77f0c542ff
morteza houshangi <34384195+houshangi@users.noreply.github.com>
2024-04-07 19:09:02 +0330

    kits houshagni
    houshangi

---
+++ train.sh
@@ -0,0 +1,33 @@
+python train.py params/swin-128bg-feat12-longer.py
+
+
+
+# python train.py params/swin-128bg-lr-1.py
+
+# python train.py params/swin-128bg-feat24.py
+##### doesn't work... python train.py params/swin-128bg-feat24-ckpt.py # continue from ckpt
+# python train.py params/swin-128bg-feat12.py
+# python train.py params/swin-128bg-feat36.py
+# python train.py params/swin-128bg-feat48.py
+
+
+# python train.py params/swin-128bg-lr.py
+# python train.py params/swin-512bg-lr.py
+# python train.py params/swin-256-2-with-background.py
+# python train.py params/swin-256-2-label-smoothing.py
+# python train.py params/swin-256-2.py # change weights
+# python train.py params/swin-256-1.py # change spacing
+# python train.py params/swin-256.py
+# python train.py params/swin-128-1-ckpt.py
+# python train.py params/swin-128-1.py # RandCropByLabelClasses
+# python train.py params/swin-128-cls_weights-3.py # smaller LR
+# python train.py params/swin-128-cls_weights-2.py # normalize weights
+# python train.py params/swin-128-cls_weights-1.py # normalize weights
+# python train.py params/swin-128-cls_weights-ckpt.py
+# python train.py params/swin-128-cls_weights.py
+# python train.py params/swin-128.py
+# python train.py params/swin.py
+# python train.py params/u2net.py
+# python train.py params/unet2.py
+# python train.py params/unet1.py
+# python train.py params/unet.py
